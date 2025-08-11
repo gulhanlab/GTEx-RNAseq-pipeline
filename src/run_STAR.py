0b00f94a6f07416361c1ca10718a50e8f4397c16
garrett-lam <glam@broadinstitute.org>
2025-08-11 16:09:14 -0400

    make quantTranscriptomeSAMoutput default to None, only available in 2.7.11b STAR version

--- src/run_STAR.py
+++ src/run_STAR.py
@@ -46,7 +46,8 @@ parser.add_argument('--outSAMattrRGline', default=['ID:rg1', 'SM:sm1'], nargs='+
 parser.add_argument('--outSAMattributes', default=['NH', 'HI', 'AS', 'nM', 'NM', 'ch'], nargs='+')
 parser.add_argument('--varVCFfile', default=None, help='VCF for the input sample; currently supports SNPs only')
 parser.add_argument('--waspOutputMode', default='SAMtag')
-parser.add_argument('--quantTranscriptomeSAMoutput', default='BanSingleEnd_BanIndels_ExtendSoftclip', help='string: alignment filtering for TranscriptomeSAM output')
+#parser.add_argument('--quantTranscriptomeSAMoutput', default='BanSingleEnd_BanIndels_ExtendSoftclip', help='string: alignment filtering for TranscriptomeSAM output')
+parser.add_argument('--quantTranscriptomeSAMoutput', default=None, help='string: alignment filtering for TranscriptomeSAM output')
 parser.add_argument('--winAnchorMultimapNmax', default='50', help='int>0: max number of loci anchors are allowed to map to')
 parser.add_argument('--genomeTransformOutput', default=None, nargs='+', help='string(s) which output to transform back to original genome')
 parser.add_argument('--chimSegmentMin', default='15', help='Minimum chimeric segment length; switches on detection of chimeric (fusion) alignments')
@@ -102,8 +103,9 @@ if args.waspOutputMode=='SAMtag' and args.varVCFfile is not None:
     if 'vW' not in args.outSAMattributes:
         args.outSAMattributes.append('vW')
         print("  * adding 'vW' tag to outSAMattributes", flush=True)
-cmd += f' --quantTranscriptomeSAMoutput {args.quantTranscriptomeSAMoutput}'\
-    + f' --winAnchorMultimapNmax {args.winAnchorMultimapNmax}'
+if args.quantTranscriptomeSAMoutput is not None:
+    cmd += f' --quantTranscriptomeSAMoutput {args.quantTranscriptomeSAMoutput}'
+cmd += f' --winAnchorMultimapNmax {args.winAnchorMultimapNmax}'
 if args.genomeTransformOutput is not None:
     cmd += f" --genomeTransformOutput {' '.join(args.genomeTransformOutput)}"
 if int(args.chimSegmentMin)>0:
